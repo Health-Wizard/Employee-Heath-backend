3c0d9c04e5e0915cff49b8792b7b7771262d2d20
Kingsuk Biswas <kingsuk2k@gmail.com>
2023-12-03 01:05:18 +0530

    added charts

---
+++ src/controllers/chartView.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+class CharView {
+  prisma = new PrismaClient();
+
+  getHealthAnalytics = async (req: any, res: Response) => {
+    const empId = parseInt(req.user.empId);
+    if (!empId) {
+      return res.status(400).json({ message: "Emp Id is missing or invalid." });
+    }
+    try {
+      const healthDetails = await this.prisma.health_Analytics.findUnique({
+        where: { empId },
+      });
+
+      if (!healthDetails) {
+        return res.status(404).json({ message: "Employee not found." });
+      }
+      console.log(healthDetails);
+
+      res.status(200).json(healthDetails.health_data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+}
+
+export default CharView;
--- src/routes/userAuth.ts
+++ src/routes/userAuth.ts
@@ -1,48 +1,54 @@
-import express from 'express';
-import UserAuthController from '../controllers/userAuthController';
-import EmployeeDetails from '../controllers/employeeDetails';
-import { authenticateJwt } from '../middlewares/userAuthMiddleWares';
+import express from "express";
+import UserAuthController from "../controllers/userAuthController";
+import EmployeeDetails from "../controllers/employeeDetails";
+import CharView from "../controllers/chartView";
+import { authenticateJwt } from "../middlewares/userAuthMiddleWares";
 const router = express.Router();
 
 const userAuthController = new UserAuthController();
 const employeeDetails = new EmployeeDetails();
+const chartView = new CharView();
 
 // Employee route
-router.route('/register').post(userAuthController.registerEmployee);
-router.route('/login').post(userAuthController.loginEmployee);
+router.route("/register").post(userAuthController.registerEmployee);
+router.route("/login").post(userAuthController.loginEmployee);
 
 // Admin route
 // router.route('/register/admin').post(userAuthController.registerAdmin);
 
 // Company Names
 router
-  .route('/companyName')
+  .route("/companyName")
   .get(authenticateJwt, employeeDetails.getUniqueCompanyNames);
 
 // Get Role
-router.route('/role/').get(authenticateJwt, employeeDetails.getRole);
+router.route("/role/").get(authenticateJwt, employeeDetails.getRole);
 
 // Employee Form route
 router
-  .route('/createEmployee')
+  .route("/createEmployee")
   .post(authenticateJwt, employeeDetails.createEmployeeDetails);
 router
-  .route('/createAdmin')
+  .route("/createAdmin")
   .post(authenticateJwt, employeeDetails.createAdminDetails);
 router
-  .route('/getEmployees')
+  .route("/getEmployees")
   .get(authenticateJwt, employeeDetails.getEmployeeDetails);
 router
-  .route('/getEmployeesByCompany')
+  .route("/getEmployeesByCompany")
   .get(authenticateJwt, employeeDetails.getEmployeesByCompany);
 router
-  .route('/getEmployee/:id')
+  .route("/getEmployee/:id")
   .get(authenticateJwt, employeeDetails.getSingleEmployee);
 router
-  .route('/deleteEmployee/:id')
+  .route("/deleteEmployee/:id")
   .delete(authenticateJwt, employeeDetails.deleteSingleEmployee);
 router
-  .route('/updateEmployee/:id')
+  .route("/updateEmployee/:id")
   .post(authenticateJwt, employeeDetails.updateSingleEmployee);
+
+router
+  .route("/getHealthData/")
+  .get(authenticateJwt, chartView.getHealthAnalytics);
 
 export default router;
